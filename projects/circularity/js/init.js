ceef8b5c0e528ee5e4c9a2172c3d20d995e82915
Dakota Lassiter <dakota.lassiter@sciacademy.org>
2023-12-04 18:12:11 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,12 +20,22 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+      var circle;
+      var circles = []
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle 
+       circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+       physikz.addRandomVelocity(circle, canvas);
+       view.addChild(circle);
+       circles.push(circle);
 
         // TODO 3 / 7 : Call the drawCircle() function 
+     drawCircle();
+     drawCircle();
+     drawCircle();
+     drawCircle();
+     drawCircle();
 
 
         ////////////////////////////////////////////////////////////
@@ -39,11 +49,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            physikz.updatePosition(myArray[0]) 
+            physikz.updatePosition(myArray[1]) 
+            physikz.updatePosition(myArray[2]) 
+            physikz.updatePosition(myArray[3]) 
+            physikz.updatePosition(myArray[4]) 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+            game.checkCirclePosition(myArray[0])
+            game.checkCirclePosition(myArray[1])
+            game.checkCirclePosition(myArray[2])
+            game.checkCirclePosition(myArray[3])
+            game.checkCirclePosition(myArray[4])
             // TODO 9 : Iterate over the array
            
             
